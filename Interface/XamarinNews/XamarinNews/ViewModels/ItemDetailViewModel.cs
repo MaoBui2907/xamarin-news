a460b56a0dc1bfbbc5adf1b341c6bd7bc05c08ab
Nguyễn Văn Tài <vantau68c1@gmail.com>
2019-12-16 09:08:55 +0700

    interface Initial

---
+++ Interface/XamarinNews/XamarinNews/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinNews.Models;
+
+namespace XamarinNews.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
