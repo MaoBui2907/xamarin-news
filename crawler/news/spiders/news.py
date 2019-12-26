b9fcdd264a9d515dc1d04cbefc851788fab759be
nguyenmao2101 <maonguyen199873@gmail.com>
2019-12-26 09:14:48 +0700

    Update crawler equally divided

--- crawler/news/spiders/news.py
+++ crawler/news/spiders/news.py
@@ -6,12 +6,18 @@ from scrapy.linkextractors import LinkExtractor
 import csv
 import os
 
-from pymongo import MongoClient 
+from pymongo import MongoClient
 
 run_config = {
     "start_urls": ['https://vnexpress.net/24h-qua'],
     "allowed_domains": ['vnexpress.net'],
-    "allowed_regex": "^https://vnexpress.net/van-hoa/|^https://vnexpress.net/suc-khoe/|^https://vnexpress.net/giao-duc/|^https://vnexpress.net/thoi-su/|^https://vnexpress.net/khoa-hoc/|^https://vnexpress.net/the-thao/",
+    "category":["doi-song","suc-khoe","giao-duc","thoi-su","khoa-hoc","the-thao"],
+    "allowed_regex": ["^https://vnexpress.net/doi-song/",
+                      "^https://vnexpress.net/suc-khoe/",
+                      "^https://vnexpress.net/giao-duc/",
+                      "^https://vnexpress.net/thoi-su/",
+                      "^https://vnexpress.net/khoa-hoc/",
+                      "^https://vnexpress.net/the-thao/"],
     "denied_extensions": ['jpg', 'png', 'pdf', 'jpeg'],
     "rm_none": True,
     "xpath_config": {
@@ -32,9 +38,11 @@ db_config = {
     "db_name": "news"
 }
 
+
 class FullLinks(CrawlSpider):
     name = 'news'
-    def __init__(self, run_config=run_config,db_config=db_config, *args, **kwargs):
+
+    def __init__(self, category="0", run_config=run_config, db_config=db_config, *args, **kwargs):
         super(FullLinks, self).__init__(*args, **kwargs)
         self.start_urls = run_config["start_urls"]
         self.allowed_domains = run_config["allowed_domains"]
@@ -42,22 +50,24 @@ class FullLinks(CrawlSpider):
         self.rm_none = run_config["rm_none"]
         self.col_titles = ["url"] + [k for k in self.xpath_config.keys()]
         self.count = 1
-        self.max_count = 500
+        self.max_count = 100
         self.db_config = db_config
+        self.category = run_config["category"][int(category)]
         client = MongoClient(host=db_config["db_host"], port=db_config["db_port"],
-                     username=db_config["db_user"], password=db_config["db_pass"],
-                     authSource=db_config["db_name"])
+                             username=db_config["db_user"], password=db_config["db_pass"],
+                             authSource=db_config["db_name"])
         db = client[db_config["db_name"]]
         self.collection = db["post"]
         self.init_len_c = self.collection.count()
-        self.collection.update_many({}, {"$set": {"trend": False}})
-        FullLinks.rules = [Rule(LinkExtractor(allow=run_config["allowed_regex"], deny_extensions=run_config["denied_extensions"]), callback="parse_item", follow=True)]
+        self.collection.update_many({"category": self.category}, {"$set": {"trend": False}})
+        FullLinks.rules = [Rule(LinkExtractor(allow=run_config["allowed_regex"][int(category)],
+                                              deny_extensions=run_config["denied_extensions"]), callback="parse_item", follow=True)]
         super(FullLinks, self)._compile_rules()
 
     def parse_item(self, response):
         record = {
             "link": response.url,
-            "category": re.search('vnexpress.net\/(.*?)\/', response.url).group(1),
+            "category": self.category,
             "trend": True
         }
         for k, v in self.xpath_config.items():
@@ -72,8 +82,6 @@ class FullLinks(CrawlSpider):
             self.collection.insert_one(record)
             print("insert " + str(self.count))
 
-        self.count  = self.count + 1
+        self.count = self.count + 1
         if self.count > self.max_count:
             raise CloseSpider("reach result's max count")
-
-
